d408f3e21d213599be8215891bace78b82d92071
Archer <zhongchuqiao1201@gmail.com>
2023-07-07 01:43:55 -0700

    carousel

--- js/detail.js
+++ js/detail.js
@@ -6,3 +6,25 @@ document.addEventListener('DOMContentLoaded', function() {
       dropdownMenu.classList.toggle('collapsed');
     });
   });
+// -------------------------------------------------------
+const images = document.querySelectorAll('.carousel-image');
+const buttons = document.querySelectorAll('.dot');
+
+function updateCarousel(imageIndex) {
+  images.forEach((image, index) => {
+    if (index === imageIndex) {
+      image.classList.add('active');
+    } else {
+      image.classList.remove('active');
+    }
+  });
+}
+
+buttons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    updateCarousel(index);
+  });
+});
+
+// 设置初始图片
+updateCarousel(0);
