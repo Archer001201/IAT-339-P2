1c554a0fc6217a05c838356c5efca5723d291361
Archer <zhongchuqiao1201@gmail.com>
2023-07-06 23:03:07 -0700

    checkout

--- js/style.js
+++ js/style.js
@@ -206,6 +206,35 @@ cart.addEventListener('click', function(event){
 //   }
 // });
 
+
+
+let numberJuniper = document.getElementById('number_juniper');
+let decrementJuniper = document.getElementById('decrement_juniper');
+let incrementJuniper = document.getElementById('increment_juniper');
+
+decrementJuniper.addEventListener('click', function() {
+    numberJuniper.value = Number(numberJuniper.value) - 1;
+});
+
+incrementJuniper.addEventListener('click', function() {
+    numberJuniper.value = Number(numberJuniper.value) + 1;
+});
+
+
+let numberAtlas = document.getElementById('number_atlas');
+let decrementAtlas = document.getElementById('decrement_atlas');
+let incrementAtlas = document.getElementById('increment_atlas');
+
+decrementAtlas.addEventListener('click', function() {
+    numberAtlas.value = Number(numberAtlas.value) - 1;
+});
+
+incrementAtlas.addEventListener('click', function() {
+    numberAtlas.value = Number(numberAtlas.value) + 1;
+});
+
+// --------------------------------- //
+
 $(document).ready(function(){
   $(".small_link a").hover(function(){
     $(".small_link a").not(this).css("color", "rgb(255,255,255)");
@@ -213,6 +242,9 @@ $(document).ready(function(){
     $(".small_link a").css("color", "black");
   });
 });
+
+
+
 
 
 
